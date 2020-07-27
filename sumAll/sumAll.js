2e85daf905548eac8efaf32e541c5770235c75bc
cotrones <tomcotroneo@gmail.com>
2020-07-27 11:56:10 -0400

    completed sumAll

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(x, y) {
+	let result = 0;
+	if (!(Number.isInteger(x)) || !(Number.isInteger(y)) || (x < 0) || (y < 0)) {
+		return 'ERROR';
+	} else {
+		if (x > y) {
+			for (let i = y; i <= x; i++) {
+				result += i;
+			}
+		} else {
+			for (let i = x; i <= y; i++) {
+				result += i;
+			}
+		}
+	}
+	return result;
 }
 
 module.exports = sumAll
