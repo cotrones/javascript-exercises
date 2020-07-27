6d41ee79234fe81bc3404e7c4253338dea537485
cotrones <tomcotroneo@gmail.com>
2020-07-27 02:12:41 -0400

    completed reverseString and removeFromArray

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(x, ...y) {
+	let arr = x;
+	let rem = y;
+	for (let i = 0; i < rem.length; i++) {
+		for (let j = 0; j < arr.length; j++) {
+			if (rem[i] === arr[j]) {
+				arr.splice(j, 1);
+			}
+		}
+	}
+	return arr;
 }
 
 module.exports = removeFromArray
--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(s) {
+	let str = String(s);
+	let arr = str.split('');
+	arr.reverse();
+	return arr.join('');
 }
 
 module.exports = reverseString
