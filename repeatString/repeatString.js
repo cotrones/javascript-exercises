55610320000daf0bb7f65e951b0e7ebadb9d9588
cotrones <tomcotroneo@gmail.com>
2020-07-27 01:16:11 -0400

    completed helloWorld and repeatString

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(s, n) {
+	let str = '';
+	let count = 0;
+	if (n < 0) {
+		return 'ERROR';
+	} else {
+		while (n > count) {
+			str+=s;
+			count++;
+		}
+		return str;
+	}
+	
 }
 
 module.exports = repeatString
