40b7366d0dc559c4094256bc11519a8208a4ea34
cotrones <tomcotroneo@gmail.com>
2020-07-27 20:41:41 -0400

    completed calculator and palindromes

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,32 +1,68 @@
-function add () {
-	
+function add (x, y) {
+	return x + y;
 }
 
-function subtract () {
-	
+function subtract (x, y) {
+	return x - y;
 }
 
-function sum () {
-	
+function sum (arr) {
+	if (arr.length < 1) {
+		return 0;
+	} else {
+		const totalSum = arr.reduce((total, num) => {
+			return total + num;
+		});
+		return totalSum;
+	}
 }
 
-function multiply () {
-	
+function multiply (arr) {
+	const multiplied = arr.reduce((total, num) => {
+		return total * num;
+	});
+	return multiplied;
 }
 
-function power() {
-	
+function power(x, y) {
+	let total = x;
+	for (let i = 1; i < y; i++) {
+		total *= x;
+	}
+	return total;
 }
 
-function factorial() {
-	
+function factorial(x) {
+	// let total = x;
+	// for (let i = x; i > 0; i--) {
+	// 	total *= x;
+	// }
+
+	let total = x =>
+		x < 0
+			? (() => {
+				throw new TypeError('No negative numbers!');
+			})
+		: x <= 1
+			? 1
+			: x * factorial(x - 1);
+	return total(x);
 }
+
+// let total = x =>
+// 	x < 0
+// 		? (() => {
+// 			throw new TypeError('No negative numbers!');
+// 		})
+// 	: x <= 1
+// 		? 1
+// 		: x * total(x - 1);
 
 module.exports = {
 	add,
 	subtract,
 	sum,
 	multiply,
-    power,
+	power,
 	factorial
 }
--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function() {
+const palindromes = function(str) {
+	const punctuation = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~ ';
+	const letters = str.toLowerCase().split('');
+	const cleanLetters = letters.filter(function(letter) {
+		return punctuation.indexOf(letter) === -1;
+	});
 
+	return cleanLetters.join('') === cleanLetters.reverse().join('');
 }
 
 module.exports = palindromes
