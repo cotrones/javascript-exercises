d8fa334f107699bad69485cb7245d71631549f2e
cotrones <tomcotroneo@gmail.com>
2020-07-27 21:42:01 -0400

    completed caesar and fibonacci

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,5 +1,23 @@
-const caesar = function() {
+const caesar = function(str, num) {
+	return str
+    .split("")
+    .map(char => shiftChar(char, num))
+    .join("");
+}
 
+const codeSet = code => (code < 97 ? 65 : 97);
+
+const mod = (n, m) => (n % m + m) % m;
+
+const shiftChar = (char, num) => {
+  const code = char.charCodeAt();
+
+  if ((code >= 65 && code <= 90) || (code >= 97 && code <= 122)) {
+    return String.fromCharCode(
+      mod(code + num - codeSet(code), 26) + codeSet(code)
+    );
+  }
+  return char;
 }
 
 module.exports = caesar
--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+	if (num < 0) {
+		return 'OOPS';
+	} else if (num < 2) {
+		return Number(num);
+	} else {
+		return fibonacci(Number(num) - 1) + fibonacci(Number(num) - 2);
+	}
 }
 
 module.exports = fibonacci
